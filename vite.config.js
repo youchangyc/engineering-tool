f53b188246b82c2081f6432247dae771037ff972
youchangyc <youchangyc6@gmail.com>
2026-05-10 23:00:31 +0800

    Use Vite DeepSeek env variable

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  envPrefix: ['VITE_', 'REACT_APP_'],
   build: {
     outDir: 'dist',
     sourcemap: false,
